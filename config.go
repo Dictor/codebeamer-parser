d797b3cc0b91229577dc5d44560ee535930339cb
Jeonghyun Kim <kimdictor@gmail.com>
2025-05-07 11:59:22 +0900

    Revert to production config

--- config.go
+++ config.go
@@ -7,19 +7,19 @@ const (
 	TreeAjaxUrl               string = "/cb/trackers/ajax/tree.spr"
 
 	// user variant setting
-	/*
-		CodebeamerHost           string = "https://ade-cb.hmckmc.co.kr"
-		FcuProjectId             int    = 119
-		FcuRequirementName       string = "소프트웨어 요구사양 FCU"
-		CodebeamerRqIconUrl      string = "/cb/displayDocument?doc_id=30320010"
-		TreeConfigDataExpression string = "tree.config.data"
-		RequirementNodeName      string = "상세 사양"
-	*/
-
-	CodebeamerHost           string = "https://codebeamer.com"
-	FcuProjectId             int    = 1005
-	FcuRequirementName       string = "작업 항목"
+	CodebeamerHost           string = "https://ade-cb.hmckmc.co.kr"
+	FcuProjectId             int    = 119
+	FcuRequirementName       string = "소프트웨어 요구사양 FCU"
 	CodebeamerRqIconUrl      string = "/cb/displayDocument?doc_id=30320010"
 	TreeConfigDataExpression string = "tree.config.data"
-	RequirementNodeName      string = "downstream ex"
+	RequirementNodeName      string = "상세 사양"
+
+	/*
+		CodebeamerHost           string = "https://codebeamer.com"
+		FcuProjectId             int    = 1005
+		FcuRequirementName       string = "작업 항목"
+		CodebeamerRqIconUrl      string = "/cb/displayDocument?doc_id=30320010"
+		TreeConfigDataExpression string = "tree.config.data"
+		RequirementNodeName      string = "downstream ex"
+	*/
 )
